136c8e1d328288bf3003692aeb50c6e243169e53
Ephemeralism <sammail@gmail.com>
2025-10-13 11:57:49 +0800

    update content

--- src/site.config.mjs
+++ src/site.config.mjs
@@ -1,14 +1,14 @@
 export default {
-    siteTitle: "The Storyteller", // Main site title displayed in header
-    siteSubTitle: "Minimal musings on code, design, and life", // Subtitle shown under main title
-    copyright: "© 2025 The Storyteller. All Rights Reserved.", // Footer copyright text
+    siteTitle: "Feedforward", // Main site title displayed in header
+    siteSubTitle: "Thoughts and exploration of AI", // Subtitle shown under main title
+    copyright: "© 2025 Samuel Lee. All Rights Reserved.", // Footer copyright text
     showAuthorsOnHomePage: false, // Display author info on homepage
     showFeaturrdPostsOnHomePage: true, // Show featured posts section on homepage
     showCategoryOnPosts: true, // Display categories on posts in homepage
     labels: {
-        featuredPosts: "Featured Posts", // Title for featured posts section
-        latestPosts: "Latest Posts", // Title for latest posts section
-        viewAllPosts: "View All Posts", // Text for link to paginated blog
+        featuredPosts: "Featured", // Title for featured posts section
+        latestPosts: "Latest", // Title for latest posts section
+        viewAllPosts: "View All", // Text for link to paginated blog
         backToHome: "Back to Home", // Back navigation text
         youMightAlsoLike: "You Might Also Like", // Similar posts section title
         postedIn: "Posted in", // Category prefix text in articles
@@ -28,16 +28,16 @@ export default {
         nextText: "Next", // Text for next page button
         postLabel: "posts", // Label used in pagination info (e.g., "8 posts")
     },
-    defaultAuthorName: "Hasin Hayder",
-    showCategoriesLinkOnFooter: true, // Show Categories link in footer
+    defaultAuthorName: "Samuel Lee",
+    showCategoriesLinkOnFooter: false, // Show Categories link in footer
     showTagsLinkOnFooter: true, // Show Tags link in footer
-    showAuthorsLinkOnFooter: true, // Show Authors link in footer
+    showAuthorsLinkOnFooter: false, // Show Authors link in footer
     showSimilarPosts: true, // Display similar posts on article pages
     showReadMoreLinkOnFeaturedPosts: true, // Show "Read More" on featured cards
     showThumbnailOnFeaturedPosts: true, // Display thumbnails on featured posts
     numberOfLatestPostsOnHomePage: 6, // Number of latest posts on homepage
     numberOfBlogPostsPerPage: 8, // Number of posts per paginated blog
 
-    gTag: "G-V5QHDKBFP" // Google Analytics tracking ID
+    gTag: "G-8QVW4V8D5S" // Google Analytics tracking ID
 }
 
