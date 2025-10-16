bde52306d4232d53a4a287bb70dad40fab88f844
ephemeralism <sammail@gmail.com>
2025-10-16 23:18:24 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs
-/partytown";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
